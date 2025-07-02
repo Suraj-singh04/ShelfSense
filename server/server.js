b35901904c00bc47ce9e15b0a1b438e1de60d674
Suraj-singh04 <surajsingh877028@gmail.com>
2025-07-02 12:13:59 +0530

    Upadated server logic

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const connectToDB = require("../database/db");
+const inventory = require("./routes/inventory");
+const products = require("./routes/product");
 
 connectToDB();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/inventory", inventory);
+app.use("/products", products);
 
 app.listen(PORT, () => {
   console.log("Server is running");
